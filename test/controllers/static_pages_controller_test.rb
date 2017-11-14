d2e92e5035201a3dd25d34046f5103de3b7601fd
stella <stellaireri@gmiail.com>
2017-11-14 19:07:29 +0300

    modify the titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | AutoLeave App"
+    assert_select "title", "AutoLeave App"
   end
 
   test "should get help" do
